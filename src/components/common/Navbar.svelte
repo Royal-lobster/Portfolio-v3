02bc9419c57c54b98415c88932608a2f0a585ba8
Srujan Gurram <srujangs8@gmail.com>
2023-07-08 16:58:20 +0530

    finish hero section 🚀

---
+++ src/components/common/Navbar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import SR from "../../assets/logos/sr.svelte";
+  import { button } from "../elements/Button";
+
+  const links = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Blog", href: "/blog" },
+    { name: "Contact", href: "/contact" },
+  ];
+</script>
+
+<header
+  class="container flex items-center justify-between mx-auto py-2 px-4 border border-neutral-800 my-4 mt-6 rounded-xl"
+>
+  <SR />
+  <nav class="flex items-center justify-between gap-4 px-4">
+    {#each links as link}
+      <a href={link.href} class={button({ type: "link" })}>
+        {link.name}
+      </a>
+    {/each}
+  </nav>
+</header>
---
+++ src/components/portfolio/sections/Hero.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { button } from "../../elements/Button";
+  import BlurryAnimation from "../elements/BlurryAnimation.svelte";
+</script>
+
+<section
+  class="flex flex-col lg:flex-row justify-between items-stretch gap-4 relative rounded-md"
+>
+  <div class="flex flex-col gap-4 max-w-xl">
+    <h1 class="text-4xl md:text-5xl font-black">
+      I craft <br /> web experiences that feels correct
+    </h1>
+    <p class="text-lg">
+      I am a full stack web developer focused on creating web applications with
+      user in mind
+    </p>
+    <div class="space-x-4">
+      <button class={button()}>Projects</button>
+      <button class={button({ type: "outline" })}>Contact</button>
+    </div>
+  </div>
+  <div class="flex flex-col w-full gap-4">
+    {#each Array(8) as _, i}
+      <BlurryAnimation index={i + 1} total={8} />
+    {/each}
+  </div>
+</section>
