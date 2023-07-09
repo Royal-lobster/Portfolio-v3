ea4574abebaab05b7d7f6dc5fa718a667204d0d3
Srujan Gurram <srujangs8@gmail.com>
2023-07-09 16:49:14 +0530

    Add AboutMe section to the homepage

---
+++ src/components/portfolio/sections/AboutMe.svelte
@@ -0,0 +1,20 @@
+<section class="flex justify-center gap-8 my-16 max-w-2xl mx-auto">
+  <h1 class="text-4xl font-black text-right">About <br /> Me !</h1>
+  <div class="h-36 w-1 border-l border-neutral-800" />
+  <div class="font-medium space-y-4">
+    <p>
+      Hello 👋 I am a 21 years old Full Stack Web developer and UI and UX
+      designer.
+    </p>
+    <p>
+      I Started coding when i was 15 with HTML and CSS, and worked on a ton of
+      projects as hobby and love towards building UIs and tools that solve real
+      problems
+    </p>
+    <p>
+      I Worked on over 100 projects over last 4 years most of them are open
+      source ! I love to teach, keeping track of new technologies to explore,
+      and don’t fear bugs 😄
+    </p>
+  </div>
+</section>
