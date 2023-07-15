559077e6185b67e0caf8916c8eaeee3596048b7a
Srujan Gurram <srujangs8@gmail.com>
2023-07-15 15:26:21 +0530

    Add ExperienceCard component and define collections for posts, projects, and experience in config.ts file

---
+++ src/components/portfolio/elements/ExperienceCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { IconBook } from "@tabler/icons-svelte";
+  import Button from "../../elements/Button/index.svelte";
+  export let startDate: string;
+  export let endDate: string;
+  export let title: string;
+  export let company: string;
+</script>
+
+<article class="border border-neutral-800 rounded-lg p-4">
+  <div>
+    <p class="mt-2">
+      {startDate} - {endDate}
+    </p>
+    <h3 class="text-2xl font-bold">{title}</h3>
+    <div class="flex items-center justify-between mt-2">
+      <p>
+        @ {company}
+      </p>
+      <Button leftIcon={IconBook} type="link">Read</Button>
+    </div>
+  </div>
+</article>
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,46 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+const posts = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    projectLink: z.string().url(),
+    githubLink: z.string().url(),
+    learned: z.array(z.string()),
+  }),
+});
+
+const experience = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    startDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    endDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+	company: z.string(),
+	companyLink: z.string().url(),
+  }),
+});
+
+export const collections = { posts, projects, experience };
