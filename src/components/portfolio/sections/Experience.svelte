460d83fc46baad46b6f2066eeab191f928aab688
Srujan Gurram <srujangs8@gmail.com>
2023-07-15 15:07:20 +0530

    Add Experience section to the portfolio homepage

---
+++ src/components/portfolio/sections/Experience.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { IconBrandRedhat } from "@tabler/icons-svelte";
+  import CommonSection from "../elements/CommonSection.svelte";
+  import ExperienceCard from "../elements/ExperienceCard.svelte";
+</script>
+
+<CommonSection title="Experience" icon={IconBrandRedhat}>
+  <ExperienceCard />
+</CommonSection>
