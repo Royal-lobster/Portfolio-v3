fb5e2a4982565c33f17a86a9c4eb0bf37bb8a239
Srujan Gurram <srujangs8@gmail.com>
2023-07-29 18:37:11 +0530

    Refactor Image component to include a dynamic class based on `className`.

--- src/components/elements/Image/index.svelte
+++ src/components/elements/Image/index.svelte
@@ -3,9 +3,10 @@
   export let src: string;
   export let alt: string;
   export let width: number;
+  export let className: string;
   export let height: number;
 </script>
 
 {#await getImage({ src, width, height }) then img}
-  <img src={img.src} {alt} {...img.attributes} />
+  <img src={img.src} {alt} {...img.attributes} class={className} />
 {/await}
